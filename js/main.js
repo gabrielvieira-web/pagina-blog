2e5bfee84e4107042ba8af6278fa1d88ff5737ea
gabrielvieira-web <vieira.github@gmail.com>
2023-06-30 13:42:45 -0300

    Versão Desktop finalizada

---
+++ js/main.js
@@ -0,0 +1,71 @@
+const menu = document.querySelector("[data-menu]");
+const campoNavegacao = document.querySelector("[data-navegacao]");
+const imagemMenuClose = "http://127.0.0.1:5500/images/icon-close.svg";
+const imagemMenuHamburguer = "http://127.0.0.1:5500/images/icon-hamburger.svg";
+
+const campoDeCategorias = document.querySelectorAll("[data-campo-categoria]");
+const categorias = document.querySelectorAll("[data-categoria]");
+const arrowDark = document.querySelectorAll("[data-arrow-dark]");
+const arrowLight = document.querySelectorAll("[data-arrow-light]");
+const campoDeItens = document.querySelectorAll("[data-campo-itens]");
+
+categorias.forEach((categoria) => categoria.addEventListener("click", evento => {
+
+    if (evento.target.innerHTML == 'Product') {
+        
+        if (campoDeItens[0].style.display == 'block') {
+            campoDeItens[0].style.display = 'none';
+            arrowDark[0].classList.remove("arrow-dark");
+            arrowLight[0].classList.remove("arrow-light");
+        } else if (campoDeItens[0].style.display == '' || campoDeItens[0].style.display == 'none') {
+            campoDeItens[0].style.display = 'block';
+            arrowDark[0].classList.add("arrow-dark");
+            arrowLight[0].classList.add("arrow-light");
+        }    
+
+    };
+    
+    if (evento.target.innerHTML == 'Company') {
+        
+        if (campoDeItens[1].style.display == 'block') {
+            campoDeItens[1].style.display = 'none';
+            arrowDark[1].classList.remove("arrow-dark");
+            arrowLight[1].classList.remove("arrow-light");
+        } else if (campoDeItens[1].style.display == '' || campoDeItens[1].style.display == 'none') {
+            campoDeItens[1].style.display = 'block';
+            arrowDark[1].classList.add("arrow-dark");
+            arrowLight[1].classList.add("arrow-light");
+        }    
+
+    };
+    
+    if (evento.target.innerHTML == 'Connect') {
+        
+        if (campoDeItens[2].style.display == 'block') {
+            campoDeItens[2].style.display = 'none';
+            arrowDark[2].classList.remove("arrow-dark");
+            arrowLight[2].classList.remove("arrow-light");
+        } else if (campoDeItens[2].style.display == '' || campoDeItens[2].style.display == 'none') {
+            campoDeItens[2].style.display = 'block';
+            arrowDark[2].classList.add("arrow-dark");
+            arrowLight[2].classList.add("arrow-light");
+        }    
+
+    };
+
+}));
+
+menu.addEventListener("click", evento => {
+    if (evento.target.src == imagemMenuHamburguer) {
+        menu.classList.add("close");
+
+        menu.src = imagemMenuClose;
+        campoNavegacao.style.display = 'block';
+
+    } else if (evento.target.src == imagemMenuClose) {
+        menu.classList.remove("close");
+
+        menu.src = imagemMenuHamburguer;
+        campoNavegacao.style.display = 'none';
+    }
+});
